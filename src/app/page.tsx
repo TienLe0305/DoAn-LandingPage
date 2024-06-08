10af314289b40b1ff56072b0298086c93890a696
Le Van Tien <lvt030501@gmail.com>
2024-06-09 03:30:35 +0700

    Update: update favicon and title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,14 @@
 import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-import Blog from "@/components/Blog";
-import Brands from "@/components/Brands";
 import ScrollUp from "@/components/Common/ScrollUp";
-import Contact from "@/components/Contact";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
-import Pricing from "@/components/Pricing";
 import Testimonials from "@/components/Testimonials";
 import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
+  title: "Nebula",
+  description: "This is Landing Page for nebulaAssistant Extension Chrome",
   // other metadata
 };
 
@@ -24,13 +19,8 @@ export default function Home() {
       <Hero />
       <Features />
       <Video />
-      {/* <Brands /> */}
       <AboutSectionOne />
-      <AboutSectionTwo />
       <Testimonials />
-      <Pricing />
-      <Blog />
-      <Contact />
     </>
   );
 }
